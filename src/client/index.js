18f05cd897fd32b13214be075b14769b1c4d46c9
Jeremie <jeremiegthr@gmail.com>
2020-08-30 15:42:53 +0200

    remove redux

--- src/client/index.js
+++ src/client/index.js
@@ -1,27 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import createLogger from 'redux-logger';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { storeStateMiddleWare } from './middleware/storeStateMiddleWare';
-import reducer from './reducers';
-import App from './containers/app';
-import { alert } from './actions/alert';
+import App from '@/containers/app';
 
-const initialState = {};
-
-const store = createStore(
-  reducer,
-  initialState,
-  applyMiddleware(thunk, createLogger()),
-);
-
-ReactDom.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('tetris'),
-);
-
-store.dispatch(alert('Soon, will be here a fantastic Tetris ...'));
+ReactDom.render(<App />, document.getElementById('tetris'));
---
+++ src/client/screens/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+  return <span>Hello World !</span>;
+}
