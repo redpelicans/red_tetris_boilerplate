d8f06f6b3fe0447e091d71cccce8da1fd431b92c
Jeremie <jeremiegthr@gmail.com>
2020-09-03 16:58:35 +0200

    fix some eslint rules

--- params.js
+++ params.js
@@ -1,9 +1,9 @@
 const params = {
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3004,
     get url() {
-      return 'http://' + this.host + ':' + this.port;
+      return `http${this.host}:${this.port}`;
     },
   },
 };
