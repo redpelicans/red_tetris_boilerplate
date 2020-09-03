8b5bd3086a6533728c7f4e037d9f9722be35c582
Jeremie <jeremiegthr@gmail.com>
2020-09-03 09:23:25 +0200

    format code

--- src/client/pages/App.js
+++ src/client/pages/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://0.0.0.0:3004";
 const socket = socketIOClient(ENDPOINT);
 
 export default function App() {
   const [piece, setPiece] = useState("");
-  
+
   useEffect(() => {
-    socket.on('piece', data => {
+    socket.on("piece", (data) => {
       console.log("I got a new piece!");
       console.log(data.response);
       setPiece(data.response);
@@ -16,22 +16,22 @@ export default function App() {
 
   const callForPiece = () => {
     console.log("I'm asking for a new piece!");
-    socket.emit('getPiece');
+    socket.emit("getPiece");
   };
 
   return (
     <div>
       <span>Hello World ! </span>
-      <br/>
+      <br />
       <span>This is a socket test !</span>
-      <br/>
-      <br/>
-      <button onClick={callForPiece}>
-        Get a piece!
-      </button>
-      <br/>
-      <br/>
-      <span>You got piece [{piece.piece}] with color [{piece.color}], congrats!</span>
+      <br />
+      <br />
+      <button onClick={callForPiece}>Get a piece!</button>
+      <br />
+      <br />
+      <span>
+        You got piece [{piece.piece}] with color [{piece.color}], congrats!
+      </span>
     </div>
-    );
+  );
 }
