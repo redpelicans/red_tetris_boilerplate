91fa60816d6c5d262c1b1534d1f139f9ee55ccec
Nicolas VIENOT <nvienot@ingima.com>
2020-09-06 18:42:41 +0200

    #4 test store players in object global

--- src/server/socket/player/handlers.js
+++ src/server/socket/player/handlers.js
@@ -1,13 +1,13 @@
 import Player from "models/player";
 import Response from "models/response";
 import { logerror, loginfo } from "utils/log";
-
+import { players } from "store";
 import { PLAYER } from "./../../../config/actions/player";
 
 export const handlerCreatePlayer = async (socket, { name }) => {
   const socketId = socket.id;
   const player = new Player({ name, socketId });
-  // players.push(player);
+  players.push(player);
   const response = Response.success(PLAYER.CREATE, player);
   loginfo("Player", response.payload.name, "created!");
   socket.emit(PLAYER.RES, { response });
--- src/server/socket/players/handlers.js
+++ src/server/socket/players/handlers.js
@@ -1,10 +1,11 @@
 import Player from "models/player";
 import Response from "models/response";
 import { logerror, loginfo } from "utils/log";
+import { players } from "store";
 import { PLAYERS } from "./../../../config/actions/players";
 
 export const handlerGetPlayers = async (socket) => {
-  const response = ["here should be all the players"];
+  const response = players;
   loginfo(response);
   socket.emit(PLAYERS.RES, { response });
 };
