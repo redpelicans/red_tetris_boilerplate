c80cd9a39073ff50f074820f29696c930846916d
Jeremie <jeremiegthr@gmail.com>
2020-09-06 22:53:54 +0200

    add alias to store dir

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,7 @@ module.exports = () => {
         hooks: path.resolve(__dirname, "src/client/hooks"),
         pages: path.resolve(__dirname, "src/client/pages"),
         reducers: path.resolve(__dirname, "src/client/reducers"),
+        store: path.resolve(__dirname, "src/client/store"),
       },
       extensions: ["*", ".js", ".jsx", ".scss"],
     },
