433cbed9eab13f4874d573e66511232149b004f8
Nicolas VIENOT <nvienot@ingima.com>
2020-09-06 17:48:50 +0200

    #4 basic socket test client

--- src/client/pages/TestSockets.js
+++ src/client/pages/TestSockets.js
@@ -4,7 +4,13 @@ const ENDPOINT = "http://0.0.0.0:3004";
 const socket = socketIOClient(ENDPOINT);
 
 export default function TestSockets() {
-  const [piece, setPiece] = useState("");
+  const [piece, setPiece] = useState([]);
+  const [playerName, setPlayerName] = useState([]);
+  const [player, setPlayer] = useState([]);
+
+  const handlePlayerName = (e) => {
+    setPlayerName(e.target.value);
+  };
 
   useEffect(() => {
     socket.on("piece:send", (data) => {
@@ -12,11 +18,27 @@ export default function TestSockets() {
       console.log(data.response);
       setPiece(data.response);
     });
+
+    socket.on("player:response", (data) => {
+      console.log("I got a new player!");
+      console.log(data.response);
+      setPlayer(data.response.payload);
+    });
   }, []);
 
   const callForPiece = () => {
     console.log("I'm asking for a new piece!");
     socket.emit("piece:get");
+  };
+
+  const callForPlayer = () => {
+    console.log("I'm asking for a new player!");
+    socket.emit("player:create", { name: playerName });
+  };
+
+  const callForPlayers = () => {
+    console.log("I'm asking for the list of the players!");
+    socket.emit("players:get");
   };
 
   return (
@@ -32,6 +54,26 @@ export default function TestSockets() {
       <span>
         You got piece [{piece.shape}] with color [{piece.color}], congrats!
       </span>
+      <br />
+      <br />
+      <input
+        name="player name"
+        label="player name"
+        onChange={handlePlayerName}
+        value={playerName}
+      />
+      <br />
+      <button onClick={callForPlayer}>Get a player!</button>
+      <br />
+      <br />
+      <span>
+        You got player [{player.name}] with id [{player.id}] and socketId [
+        {player.socketId}], congrats!
+      </span>
+      <br />
+      <br />
+      <button onClick={callForPlayers}>Trigger players:get!</button>
+      <br />
     </div>
   );
 }
