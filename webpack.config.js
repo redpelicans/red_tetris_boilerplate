43a0c06057d5ae6159806ba617b14078d630ec89
Jeremie <jeremiegthr@gmail.com>
2020-09-10 18:55:36 +0200

    setting a proper config for files && use native cache for babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const CLIENT_DIR = "src/client";
+
 module.exports = () => {
   const isEnvProduction = process.env.NODE_ENV === "production";
 
   return {
     mode: isEnvProduction ? "production" : "development",
 
-    entry: path.join(__dirname, "src/client/index.js"),
+    entry: path.join(__dirname, CLIENT_DIR, "index.js"),
 
     output: {
       path: path.join(__dirname, "build"),
@@ -24,16 +26,21 @@ module.exports = () => {
       rules: [
         {
           test: /\.(js|jsx)$/,
-          include: path.resolve(__dirname, "src/client"),
-          use: ["cache-loader", "babel-loader"],
+          include: path.resolve(__dirname, CLIENT_DIR),
+          loader: require.resolve("babel-loader"),
+          options: {
+            cacheDirectory: true,
+            cacheCompression: false,
+          },
         },
         {
           test: /\.html$/,
+          include: path.resolve(__dirname, "public"),
           use: "html-loader",
         },
         {
           test: /\.scss$/,
-          include: path.resolve(__dirname, "src/client"),
+          include: path.resolve(__dirname, CLIENT_DIR),
           use: [
             MiniCssExtractPlugin.loader,
             "cache-loader",
@@ -43,11 +50,13 @@ module.exports = () => {
           ],
         },
         {
-          test: /\.(png|svg|jpg|gif)$/,
+          test: /\.(png|svg|jpe?g|gif)$/,
+          include: path.resolve(__dirname, CLIENT_DIR, "assets/img"),
           use: ["cache-loader", "file-loader"],
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
+          include: path.resolve(__dirname, CLIENT_DIR, "assets/fonts"),
           use: ["cache-loader", "file-loader"],
         },
       ],
@@ -56,12 +65,12 @@ module.exports = () => {
     resolve: {
       symlinks: false,
       alias: {
-        actions: path.resolve(__dirname, "src/client/actions"),
-        components: path.resolve(__dirname, "src/client/components"),
-        hooks: path.resolve(__dirname, "src/client/hooks"),
-        pages: path.resolve(__dirname, "src/client/pages"),
-        reducers: path.resolve(__dirname, "src/client/reducers"),
-        store: path.resolve(__dirname, "src/client/store"),
+        actions: path.resolve(__dirname, CLIENT_DIR, "actions"),
+        components: path.resolve(__dirname, CLIENT_DIR, "components"),
+        hooks: path.resolve(__dirname, CLIENT_DIR, "hooks"),
+        pages: path.resolve(__dirname, CLIENT_DIR, "pages"),
+        reducers: path.resolve(__dirname, CLIENT_DIR, "reducers"),
+        store: path.resolve(__dirname, CLIENT_DIR, "store"),
       },
       extensions: ["*", ".js", ".jsx", ".scss"],
     },
@@ -71,10 +80,33 @@ module.exports = () => {
         filename: isEnvProduction ? "[name].css" : "styles.css",
         chunkFilename: isEnvProduction ? "[id].css" : "styles.css",
       }),
-      new HtmlWebPackPlugin({
-        template: "./index.html",
-        filename: "./index.html",
-      }),
+
+      // Generates an `index.html` file with the <script> injected.
+      new HtmlWebPackPlugin(
+        Object.assign(
+          {},
+          {
+            inject: true,
+            template: path.resolve(__dirname, "public/index.html"),
+          },
+          isEnvProduction
+            ? {
+                minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
+              }
+            : undefined,
+        ),
+      ),
     ],
   };
 };
