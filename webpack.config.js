d215f2976978b6ff41a554a6022db9309afc5df3
Jeremie <jeremiegthr@gmail.com>
2020-09-12 08:46:17 +0200

    enhanced webpack drastically -- faster, and more friendly

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
+const CleanTerminalPlugin = require("clean-terminal-webpack-plugin");
 
-const CLIENT_DIR = "src/client";
+const CLIENT_DIR = path.join(__dirname, "src/client");
 
 module.exports = () => {
   const isEnvProduction = process.env.NODE_ENV === "production";
@@ -11,7 +12,7 @@ module.exports = () => {
   return {
     mode: isEnvProduction ? "production" : "development",
 
-    entry: path.join(__dirname, CLIENT_DIR, "index.js"),
+    entry: path.join(CLIENT_DIR, "index.js"),
 
     output: {
       path: path.join(__dirname, "build"),
@@ -23,13 +24,31 @@ module.exports = () => {
 
     devServer: {
       historyApiFallback: true,
+      stats: "minimal",
     },
 
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
-          include: path.resolve(__dirname, CLIENT_DIR),
+          include: path.resolve(CLIENT_DIR),
+          enforce: "pre",
+          use: [
+            {
+              options: {
+                cache: true,
+                formatter: require.resolve("eslint-friendly-formatter"),
+                eslintPath: require.resolve("eslint"),
+                configFile: ".eslintrc",
+                resolvePluginsRelativeTo: __dirname,
+              },
+              loader: require.resolve("eslint-loader"),
+            },
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          include: path.resolve(CLIENT_DIR),
           loader: require.resolve("babel-loader"),
           options: {
             cacheDirectory: true,
@@ -39,28 +58,28 @@ module.exports = () => {
         {
           test: /\.html$/,
           include: path.resolve(__dirname, "public"),
-          use: "html-loader",
+          use: require.resolve("html-loader"),
         },
         {
           test: /\.scss$/,
-          include: path.resolve(__dirname, CLIENT_DIR),
+          include: path.resolve(CLIENT_DIR),
           use: [
             MiniCssExtractPlugin.loader,
-            "cache-loader",
-            "css-loader",
-            "postcss-loader",
-            "sass-loader",
+            require.resolve("cache-loader"),
+            require.resolve("css-loader"),
+            require.resolve("postcss-loader"),
+            require.resolve("sass-loader"),
           ],
         },
         {
           test: /\.(png|svg|jpe?g|gif)$/,
-          include: path.resolve(__dirname, CLIENT_DIR, "assets/img"),
-          use: "file-loader",
+          include: path.resolve(CLIENT_DIR, "assets/img"),
+          use: require.resolve("file-loader"),
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          include: path.resolve(__dirname, CLIENT_DIR, "assets/fonts"),
-          use: "file-loader",
+          include: path.resolve(CLIENT_DIR, "assets/fonts"),
+          use: require.resolve("file-loader"),
         },
       ],
     },
@@ -68,18 +87,19 @@ module.exports = () => {
     resolve: {
       symlinks: false,
       alias: {
-        actions: path.resolve(__dirname, CLIENT_DIR, "actions"),
-        components: path.resolve(__dirname, CLIENT_DIR, "components"),
-        helpers: path.resolve(__dirname, CLIENT_DIR, "helpers"),
-        hooks: path.resolve(__dirname, CLIENT_DIR, "hooks"),
-        pages: path.resolve(__dirname, CLIENT_DIR, "pages"),
-        reducers: path.resolve(__dirname, CLIENT_DIR, "reducers"),
-        store: path.resolve(__dirname, CLIENT_DIR, "store"),
+        actions: path.resolve(CLIENT_DIR, "actions"),
+        components: path.resolve(CLIENT_DIR, "components"),
+        helpers: path.resolve(CLIENT_DIR, "helpers"),
+        hooks: path.resolve(CLIENT_DIR, "hooks"),
+        pages: path.resolve(CLIENT_DIR, "pages"),
+        reducers: path.resolve(CLIENT_DIR, "reducers"),
+        store: path.resolve(CLIENT_DIR, "store"),
       },
       extensions: ["*", ".js", ".jsx", ".scss"],
     },
 
     plugins: [
+      new CleanTerminalPlugin(),
       new ErrorOverlayPlugin(),
       new MiniCssExtractPlugin({
         filename: isEnvProduction ? "[name].css" : "styles.css",
