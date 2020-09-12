05eec2fd2ea8db2a13225ec5959c589c10c3002c
Jeremie <jeremiegthr@gmail.com>
2020-09-12 08:46:37 +0200

    fix some eslint warnings

--- src/client/actions/alert.js
+++ src/client/actions/alert.js
@@ -1,8 +1,6 @@
-export const ALERT_POP = 'ALERT_POP';
+export const ALERT_POP = "ALERT_POP";
 
-export const alert = (message) => {
-  return {
-    type: ALERT_POP,
-    message,
-  };
-};
+export const alert = (message) => ({
+  type: ALERT_POP,
+  message,
+});
