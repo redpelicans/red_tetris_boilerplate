446f1b034d02070cf6f66cde4e63e0d9d9df2f81
Jeremie <jeremiegthr@gmail.com>
2020-09-13 14:00:03 +0200

    WIP tetris in game dedicated hooks

--- src/client/hooks/tetris-game/useTetrisGame.js
+++ src/client/hooks/tetris-game/useTetrisGame.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { insertPiece, moveDown } from "./pieces";
+import useCurrentPiece from "./useCurrentPiece";
+import { insertPiece, moveDown, putTetromino } from "./pieces";
+import { isEmpty } from "helpers/functional";
+import { GameContext } from "store";
+import { putColor, popPiece } from "actions/pieces";
+import MOCK_TETROMINOES from "mocks/Tetrominoes";
 
 /*
  ** This custom hook is used to manage the game board.
@@ -9,27 +14,46 @@ import { insertPiece, moveDown } from "./pieces";
  **  - movePieceDown: A method to move the current piece one row down.
  */
 function useTetrisGame(cols = 10, rows = 20) {
+  const { state, dispatch } = React.useContext(GameContext);
+  const [currentPiece, updateCurrentPiece] = useCurrentPiece();
+  // const currentPiece = React.useRef(null);
+
   // The board game and its initialization
-  const [tetrisGrid, setTetrisGrid] = React.useState([]);
-  React.useEffect(() => {
+  const [tetrisGrid, setTetrisGrid] = React.useState(() => {
     try {
-      const initGrid = createGrid(cols, rows);
-      setTetrisGrid(initGrid);
+      return createGrid(cols, rows);
     } catch (err) {
       console.error(err);
+      return createGrid(10, 20);
     }
-  }, []);
+  });
+
+  const midGrid = React.useMemo(() => getMidGrid(cols), [cols]);
 
   // Methods
-  const insertPosition = React.useMemo(() => getInsertPosition(cols), [cols]);
-  const insertNewPiece = (piece) => {
-    const newGrid = insertPiece(piece, tetrisGrid, insertPosition);
+  const insertNewPiece = (piece, grid = tetrisGrid) => {
+    const newGrid = insertPiece(piece, grid, midGrid);
     setTetrisGrid(newGrid);
   };
 
+  React.useEffect(() => {
+    if (state.nextPieces.length === 4) {
+      updateCurrentPiece();
+      // console.log(currentPiece, state.nextPieces);
+      insertNewPiece(currentPiece.current.shape);
+    }
+  }, [state.nextPieces]);
+
   const movePieceDown = () => {
-    const newGrid = moveDown(tetrisGrid, cols);
-    setTetrisGrid(newGrid);
+    let newGrid = moveDown(tetrisGrid, cols);
+    // console.log(newGrid);
+    if (isEmpty(newGrid)) {
+      newGrid = putTetromino(currentPiece.current, tetrisGrid);
+      updateCurrentPiece();
+      insertNewPiece(currentPiece.current.shape, newGrid);
+    } else {
+      setTetrisGrid(newGrid);
+    }
   };
 
   return { tetrisGrid, insertNewPiece, movePieceDown };
@@ -44,6 +68,15 @@ function createGrid(col, row) {
   return Array.from(Array(row), () => new Array(col).fill(0));
 }
 
-function getInsertPosition(colLength) {
-  return Math.floor(colLength / 2) - 1;
+function getMidGrid(colLength) {
+  return Math.floor(colLength / 2);
+}
+
+// Function for test env
+function fetchFromMock(n) {
+  if (MOCK_TETROMINOES.length < n) {
+    console.log("END OF TETROMINOES");
+  }
+
+  return MOCK_TETROMINOES.splice(0, n);
 }
