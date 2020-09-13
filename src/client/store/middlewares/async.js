37573f254349e6ce999f45a5f729dd281c8e88e0
Jeremie <jeremiegthr@gmail.com>
2020-09-13 13:57:13 +0200

    to debug later

--- src/client/store/middlewares/async.js
+++ src/client/store/middlewares/async.js
@@ -1,17 +1,20 @@
 function isPromise(value) {
-  console.log(value);
+  // console.log("isPromise value >>>", value);
   const resolved = Promise.resolve(value);
-  console.log(resolved);
+  // console.log("isPromise resolved", resolved);
   return Promise.resolve(value) === value;
 }
 
+// TODO: test needed -- not sure it works as I thought
 export default function middleware(dispatch) {
   return (action) => {
     if (isPromise(action.payload)) {
+      // console.log("IS PROMISE");
       action.payload.then((v) => {
         dispatch({ type: action.type, payload: v });
       });
     } else {
+      // console.log("IS NOT PROMISE");
       dispatch(action);
     }
   };
