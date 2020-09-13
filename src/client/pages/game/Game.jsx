4b6c70e82c74a1ae6ee284649a8cf5ada657687a
Jeremie <jeremiegthr@gmail.com>
2020-09-13 20:49:09 +0200

    UI for Game

--- src/client/pages/game/Game.jsx
+++ src/client/pages/game/Game.jsx
@@ -3,34 +3,30 @@ import FlexBox from "components/flexbox/FlexBox";
 import TetrisGrid from "components/tetris/Grid";
 import { useTetrisGame } from "hooks";
 import { Link } from "react-router-dom";
-
-const MOCK_TETROMINO_I = [
-  [0, 0, 0, 0],
-  [1, 1, 1, 1],
-  [0, 0, 0, 0],
-  [0, 0, 0, 0],
-];
+import NextPieces from "./NextPieces";
+// import { GameContext } from "store";
 
 export default function Game() {
   const { tetrisGrid, ...tetris } = useTetrisGame(10, 20);
 
   return (
     <FlexBox
+      direction="col"
       width={"full"}
       height={"full"}
       className="justify-center items-center"
     >
       <Link to="/">Retour accueil</Link>
-      <FlexBox
-        direction="col"
-        width={64}
-        className="justify-center align-center"
-      >
-        <TetrisGrid grid={tetrisGrid} />
-        <button onClick={() => tetris.insertNewPiece(MOCK_TETROMINO_I)}>
-          new piece
-        </button>
-        <button onClick={tetris.movePieceDown}>go down</button>
+      <FlexBox direction="row" className="mt-4">
+        <FlexBox
+          direction="col"
+          width={64}
+          className="justify-center align-center"
+        >
+          <TetrisGrid grid={tetrisGrid} />
+          <button onClick={() => tetris.movePiece("DOWN")}>go down</button>
+        </FlexBox>
+        <NextPieces />
       </FlexBox>
     </FlexBox>
   );
