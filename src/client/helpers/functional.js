34d1380cf93b62b4a25ede83d99afb04c73965de
Jeremie <jeremiegthr@gmail.com>
2020-09-15 20:37:47 +0200

    remove comments

--- src/client/helpers/functional.js
+++ src/client/helpers/functional.js
@@ -2,16 +2,13 @@ export const shallowCopy = (element) => element.slice(0);
 
 export const deepCopy = (element) => {
   if (typeof element !== "object" || element === null) {
-    return element; // Return the value if element is not an object
+    return element;
   }
 
-  // Create an array or object to hold the values
   const copy = Array.isArray(element) ? [] : {};
 
   for (const key in element) {
     const value = element[key];
-
-    // Recursively (deep) copy for nested objects, including arrays
     copy[key] = deepCopy(value);
   }
 
