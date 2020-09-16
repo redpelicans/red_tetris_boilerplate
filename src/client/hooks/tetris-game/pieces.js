10c0508445ce22636aaa629ce0eba96c94aed69c
Jeremie <jeremiegthr@gmail.com>
2020-09-16 22:31:49 +0200

    refactor in smaller

--- src/client/hooks/tetris-game/pieces.js
+++ src/client/hooks/tetris-game/pieces.js
@@ -1,66 +1,77 @@
-import { shallowCopy } from "helpers/functional";
 import * as Grid from "./grid";
 
 export function insertPiece(piece, grid, midGrid) {
-  const gridCopy = shallowCopy(grid);
   const insertPos =
     midGrid - Math.ceil((piece.shape[0].length - piece.padding.x) / 2);
   const newPiece = {
     ...piece,
     coord: { x: insertPos, y: 0 - piece.padding.y },
   };
-  const { shape, padding, coord } = newPiece;
 
   if (!Grid.canPutLayer(grid, newPiece)) {
     return null;
   }
 
-  const colLength = shape[0].length - padding.x;
-  const rowLength = shape.length - padding.y;
-  for (let col = padding.x; col < colLength; col++) {
-    for (let row = padding.y; row < rowLength; row++) {
-      if (Grid.isPartOfPiece(shape[row][col])) {
-        gridCopy[row - padding.y][coord.x + col - padding.x] = shape[row][col];
-      }
-    }
-  }
+  const gridCopy = Grid.writePieceInGrid(grid, newPiece);
   return [gridCopy, newPiece];
 }
 
-export function moveDown(grid, colLength, rowLength, piece) {
-  const gridCopy = shallowCopy(grid);
+export function moveDown(grid, piece) {
   const newPiece = {
     ...piece,
     coord: { ...piece.coord, y: piece.coord.y + 1 },
   };
 
-  const check = Grid.canPutLayer(gridCopy, newPiece);
-  if (!check) {
+  const cleanGrid = Grid.clearPieceFromGrid(grid);
+  if (!Grid.canPutLayer(cleanGrid, newPiece)) {
     return null;
   }
 
-  const upperRowLimit = piece.coord.y >= 0 ? piece.coord.y : 0;
-  for (let col = piece.coord.x; col < colLength; col++) {
-    for (let row = rowLength - 1; row >= upperRowLimit; row--) {
-      if (Grid.isPartOfPiece(gridCopy[row][col])) {
-        gridCopy[row][col] = 0;
-        gridCopy[row + 1][col] = 1;
-      }
-    }
-  }
-  return [gridCopy, newPiece];
+  const newGrid = Grid.writePieceInGrid(cleanGrid, newPiece);
+  return [newGrid, newPiece];
 }
 
-export function putTetromino(tetromino, grid) {
-  const gridCopy = shallowCopy(grid);
-
-  return gridCopy.map((row) =>
-    row.map((col) => (col === 1 ? tetromino.color : col)),
-  );
-}
-
-function rotate(piece) {
+function rotation(piece) {
   return piece[0].map((_, colIndex) =>
     piece.map((row) => row[colIndex]).reverse(),
   );
+}
+
+function getPadding(shape) {
+  const getPaddingTop = () => {
+    for (let row = 0; row < shape.length; row++) {
+      if (!shape[row].every((col) => col === 0)) {
+        return row;
+      }
+    }
+  };
+
+  const getPaddingLeft = () => {
+    for (let col = 0; col < shape[0].length; col++) {
+      if (!shape.every((_, rowIndex) => shape[rowIndex][col] === 0)) {
+        return col;
+      }
+    }
+  };
+
+  return { x: getPaddingLeft(), y: getPaddingTop() };
+}
+
+export function rotatePiece(piece, grid) {
+  const newShape = rotation(piece.shape);
+  const newPadding = getPadding(newShape);
+  const newPiece = {
+    ...piece,
+    shape: newShape,
+    padding: newPadding,
+    dim: { width: piece.dim.height, height: piece.dim.width },
+  };
+
+  const cleanGrid = Grid.clearPieceFromGrid(grid);
+  if (!Grid.canPutLayer(cleanGrid, newPiece)) {
+    return null;
+  }
+
+  const newGrid = Grid.writePieceInGrid(cleanGrid, newPiece);
+  return [newGrid, newPiece];
 }
