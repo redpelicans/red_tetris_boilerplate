cad589c9b760bff2846b0caf040c8b81f65e7ddc
Jeremie <jeremiegthr@gmail.com>
2020-09-16 22:30:14 +0200

    fix logical errors

--- src/client/hooks/tetris-game/grid.js
+++ src/client/hooks/tetris-game/grid.js
@@ -1,9 +1,14 @@
+import { shallowCopy } from "helpers/functional";
+
+const FREE = 0;
+const CURRENT_PIECE = 1;
+
 export function isPartOfPiece(element) {
-  return element === 1;
+  return element === CURRENT_PIECE;
 }
 
 export function isFree(grid, row, col) {
-  return [0, 1].includes(grid[row][col]);
+  return grid[row][col] === FREE;
 }
 
 export function isBottomLine(grid, row) {
@@ -15,22 +20,54 @@ export function isBorderBottom(grid, row) {
 }
 
 export function canPutLayer(grid, piece) {
-  const { shape, padding, coord } = piece;
+  const { shape, padding, coord, dim } = piece;
 
-  const colLength = shape[0].length - padding.x;
-  const rowLength = shape.length - padding.y;
+  if (coord.y + dim.height > grid.length) {
+    return false;
+  }
+
+  const colLength = shape[0].length;
+  const rowLength = shape.length;
 
   for (let col = padding.x; col < colLength; col++) {
     for (let row = padding.y; row < rowLength; row++) {
-      if (isPartOfPiece(shape[row][col])) {
-        if (isBorderBottom(grid, coord.y + row)) {
-          return false;
-        }
-        if (!isFree(grid, coord.y + row, coord.x + col)) {
-          return false;
-        }
+      if (
+        isPartOfPiece(shape[row][col]) &&
+        (isBorderBottom(grid, coord.y + row) ||
+          !isFree(grid, coord.y + row, coord.x + col))
+      ) {
+        return false;
       }
     }
   }
   return true;
+}
+
+export function bindPieceToGrid(grid, piece) {
+  return grid.map((row) =>
+    row.map((col) => (col === CURRENT_PIECE ? piece.color : col)),
+  );
+}
+
+export function clearPieceFromGrid(grid) {
+  return grid.map((row) =>
+    row.map((col) => (col === CURRENT_PIECE ? FREE : col)),
+  );
+}
+
+export function writePieceInGrid(grid, piece) {
+  const { shape, padding, coord } = piece;
+  const newGrid = shallowCopy(grid);
+
+  const colLength = shape[0].length;
+  const rowLength = shape.length;
+
+  for (let col = padding.x; col < colLength; col++) {
+    for (let row = padding.y; row < rowLength; row++) {
+      if (isPartOfPiece(shape[row][col])) {
+        newGrid[coord.y + row][coord.x + col] = CURRENT_PIECE;
+      }
+    }
+  }
+  return newGrid;
 }
