9ff5ef0b611dea5c4a8d08529f707a5bba2c4c4a
nicolasvienot <nvienot@student.42.fr>
2020-09-19 00:07:34 +0200

    Clean store/players

--- src/server/store/lobbies.js
+++ src/server/store/lobbies.js
@@ -12,12 +12,12 @@ export const getLobby = async (id) => {
 export const pushLobby = async (lobby, socketId) => {
   const lobbies = (await getComplexObjectFromRedis("lobbies")) ?? {};
 
-  const alreadyOnLobby = checkIfPlayerIsOnLobbyBySocketId(lobbies, socketId);
+  const alreadyOnLobby = playerIsOnLobbyBySocketId(lobbies, socketId);
   if (alreadyOnLobby) {
     return Response.error(LOBBIES.ADD, "You already have an active lobby !");
   }
 
-  const nameTaken = checkIfLobbyNameTaken(lobbies, lobby.name);
+  const nameTaken = isLobbyNameTaken(lobbies, lobby.name);
   if (nameTaken) {
     return Response.error(LOBBIES.ADD, "lobbyName is not available!");
   }
@@ -45,14 +45,14 @@ export const popLobby = async (lobbyId, ownerId) => {
 
   delete lobbies[lobbyId];
   await setComplexObjectToRedis("lobbies", lobbies);
-  // to check payload
+  /* Payload needed? */
   return Response.success(LOBBIES.DELETE, {});
 };
 
 export const joinLobby = async (player, lobbyId) => {
   const lobbies = (await getComplexObjectFromRedis("lobbies")) ?? {};
 
-  const alreadyOnLobby = checkIfPlayerIsOnLobby(lobbies, player.id);
+  const alreadyOnLobby = playerIsOnLobbyByPlayerId(lobbies, player.id);
   if (alreadyOnLobby) {
     return Response.error(LOBBY.SUBSCRIBE, "You already are in another lobby!");
   }
@@ -62,7 +62,7 @@ export const joinLobby = async (player, lobbyId) => {
     return Response.error(LOBBY.SUBSCRIBE, "Lobby doesn't exists!");
   }
 
-  const lobbyFull = checkIfLobbyIsFull(lobby);
+  const lobbyFull = isLobbyFull(lobby);
   if (lobbyFull) {
     return Response.error(LOBBY.SUBSCRIBE, "The lobby is full!");
   }
@@ -88,7 +88,7 @@ export const leaveLobby = async (playerId, lobbyId) => {
     if (response.type === "success") {
       return Response.success(LOBBY.UNSUBSCRIBE, {});
     } else {
-      // Mouais?
+      /* Something to handle? */
       return Response.error(
         LOBBY.UNSUBSCRIBE,
         "You are the last player but not the owner there is a problem!",
@@ -99,7 +99,7 @@ export const leaveLobby = async (playerId, lobbyId) => {
   const owner = isOwner(lobby, playerId);
   if (owner) {
     lobby.owner = getNextOwner(lobby.players, playerId);
-    // dispatch the info?
+    /* Handle info on front? */
   }
 
   const newPlayers = deletePlayerFromPlayers(lobby.players, playerId);
@@ -113,7 +113,8 @@ export const leaveLobby = async (playerId, lobbyId) => {
 
 export const clearPlayerFromLobbies = async (playerId) => {
   const lobbies = (await getComplexObjectFromRedis("lobbies")) ?? {};
-  const isOnLobby = checkIfPlayerIsOnLobbyByPlayerId(lobbies, playerId);
+
+  const isOnLobby = playerIsOnLobbyByPlayerId(lobbies, playerId);
   if (isOnLobby) {
     const lobbyId = getLobbyIdByPlayerId(lobbies, playerId);
     const response = await leaveLobby(playerId, lobbyId);
@@ -124,31 +125,24 @@ export const clearPlayerFromLobbies = async (playerId) => {
   return null;
 };
 
-const checkIfLobbyNameTaken = (lobbies, name) => {
+const isLobbyNameTaken = (lobbies, name) => {
   const res = Object.keys(lobbies).some((key) => lobbies[key].name === name);
   return res;
 };
 
-const checkIfLobbyIsFull = (lobby) => {
+const isLobbyFull = (lobby) => {
   const maxPlayer = lobby?.maxPlayer;
   const nbPlayers = lobby.players.length;
   return maxPlayer <= nbPlayers;
 };
 
-const checkIfPlayerIsOnLobby = (lobbies, playerId) => {
-  const res = Object.keys(lobbies).some((key) =>
-    lobbies[key].players.some((player) => player.id === playerId),
-  );
-  return res;
-};
-
-const checkIfPlayerIsOnLobbyBySocketId = (lobbies, socketId) => {
+const playerIsOnLobbyBySocketId = (lobbies, socketId) => {
   return Object.keys(lobbies).some((key) =>
     lobbies[key].players.some((player) => player.socketId === socketId),
   );
 };
 
-const checkIfPlayerIsOnLobbyByPlayerId = (lobbies, playerId) => {
+const playerIsOnLobbyByPlayerId = (lobbies, playerId) => {
   return Object.keys(lobbies).some((key) =>
     lobbies[key].players.some((player) => player.id === playerId),
   );
@@ -175,18 +169,4 @@ const getLobbyIdByPlayerId = (lobbies, playerId) => {
     lobbies[key].players.find((player) => player.id === playerId),
   );
   return lobbyId;
-};
-
-const checkIfOwnerHasLobby = (lobbies, socketId) => {
-  const res = Object.keys(lobbies).some(
-    (key) => lobbies[key]?.owner?.socketId === socketId,
-  );
-  return res;
-};
-
-const checkIfPlayerIsOwner = (lobbies, playerId) => {
-  const res = Object.keys(lobbies).some(
-    (key) => lobbies[key]?.owner?.id === playerId,
-  );
-  return res;
 };
