99bf489a36c24b1cd438b3c4022d0e79ba8711ea
Jeremie <jeremiegthr@gmail.com>
2020-09-19 16:38:27 +0200

    add hardDrop to index

--- src/client/hooks/tetris-game/pieces/index.js
+++ src/client/hooks/tetris-game/pieces/index.js
@@ -2,3 +2,4 @@ export { default as insertion } from "./insertion";
 export { default as lateralMove } from "./lateralMove";
 export { default as rotation } from "./rotation";
 export { default as softDrop } from "./softDrop";
+export { default as hardDrop } from "./hardDrop";
