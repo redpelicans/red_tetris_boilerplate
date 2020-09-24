48e6201d992330fad3dc6077b17d3f7b97584370
Jeremie <jeremiegthr@gmail.com>
2020-09-24 16:50:43 +0200

    move non-FP functions in common helper file

--- src/client/helpers/common.js
+++ src/client/helpers/common.js
@@ -9,3 +9,18 @@ export function randomNumber(max) {
 export function randomPick(list) {
   return list[randomNumber(list.length)];
 }
+export const isEmpty = (element) => element === null || element?.length === 0;
+
+export const throttle = (func, wait) => {
+  let waiting;
+
+  return function wrapper() {
+    const ctx = this;
+
+    if (!waiting) {
+      Reflect.apply(func, ctx, arguments);
+      waiting = true;
+      setTimeout(() => (waiting = false), wait);
+    }
+  };
+};
