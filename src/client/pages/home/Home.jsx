a7bb4cedb7daa7bf0367478bb3214a1d75c29d90
Jeremie <jeremiegthr@gmail.com>
2020-09-26 21:29:27 +0200

    add Home page

--- src/client/pages/home/Home.jsx
+++ src/client/pages/home/Home.jsx
@@ -1,23 +1,55 @@
 import React from "react";
 import FlexBox from "components/flexbox/FlexBox";
 import { Link } from "react-router-dom";
-import { StoreContext } from "store";
+import GamePad from "components/game-pad/GamePad";
+import {
+  gamePadMatrix,
+  gamePadMatrixHover,
+} from "components/game-pad/constants";
+import Hoverable from "components/hoverable/Hoverable";
+import Modal from "components/modals/Modal";
+import "./Home.scss";
 
 export default function Home() {
-  const { state, dispatch } = React.useContext(StoreContext);
-
-  console.log(state.socket);
-
   return (
-    <FlexBox height="full" className="items-center justify-center">
-      <FlexBox direction="col" className="">
-        <Link
-          to="/game"
-          className="font-semibold text-xl border-2 p-4 border-red-300 rounded"
-        >
-          Jouer a Tetris
+    <FlexBox height="full" width="full">
+      <Modal className="home-modal">
+        <h1>Red Tetris</h1>
+        <Link to="/game" className="mb-10">
+          <SinglePlayer />
         </Link>
-      </FlexBox>
+        <Link to="/game">
+          <MultiPlayer />
+        </Link>
+      </Modal>
     </FlexBox>
   );
 }
+
+const SinglePlayer = () => (
+  <Hoverable className="home-game-pad">
+    <Hoverable.In className="hovered">
+      <GamePad model={gamePadMatrixHover} size={2} />
+      <h2>Solo</h2>
+    </Hoverable.In>
+    <Hoverable.Out>
+      <GamePad model={gamePadMatrix} size={2} />
+    </Hoverable.Out>
+  </Hoverable>
+);
+
+const MultiPlayer = () => (
+  <Hoverable className="home-game-pad">
+    <Hoverable.In className="hovered">
+      <FlexBox direction="row">
+        <GamePad className="mr-2" model={gamePadMatrixHover} size={2} />
+        <GamePad model={gamePadMatrixHover} size={2} />
+      </FlexBox>
+      <h2>Multijoueur</h2>
+    </Hoverable.In>
+    <Hoverable.Out className="flex flex-row">
+      <GamePad className="mr-2" model={gamePadMatrix} size={2} />
+      <GamePad model={gamePadMatrix} size={2} />
+    </Hoverable.Out>
+  </Hoverable>
+);
