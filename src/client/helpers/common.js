dd1b663bb4be35f0437935e1f637e95dfa028ef4
Jeremie <jeremiegthr@gmail.com>
2020-09-30 15:29:24 +0200

    add random methods in helpers

--- src/client/helpers/common.js
+++ src/client/helpers/common.js
@@ -1,3 +1,11 @@
 export function timeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function randomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+export function randomPick(list) {
+  return list[randomNumber(list.length)];
+}
