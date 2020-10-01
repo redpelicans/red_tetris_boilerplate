2813437759af140de5a2e1793d07627bf8579a1d
nicolasvienot <nvienot@student.42.fr>
2020-10-01 11:54:42 +0200

    Remove close on modal and change to isOpen on Overlay

--- src/client/components/modals/Modal.jsx
+++ src/client/components/modals/Modal.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export default function Modal({ children, className = "" }) {
   return (
     <FlexBox className="modal fixed inset-0 items-center justify-center z-50">
-      <div className="absolute inset-0 bg-black opacity-25" onClick={close} />
+      <div className="absolute inset-0 bg-black opacity-25" />
       <div className={"relative " + className}>{children}</div>
     </FlexBox>
   );
--- src/client/components/overlay/Overlay.jsx
+++ src/client/components/overlay/Overlay.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Modal from "components/modals/Modal";
 import PropTypes from "prop-types";
 
-export default function Overlay({ children, open, close }) {
+export default function Overlay({ children, isOpen, close }) {
   React.useEffect(() => {
     const onEscape = (evt) => {
-      if (open && evt.keyCode === 27) {
+      if (isOpen && evt.keyCode === 27) {
         close();
       }
     };
@@ -17,11 +17,11 @@ export default function Overlay({ children, open, close }) {
     };
   }, []);
 
-  return open ? <Modal>{children}</Modal> : null;
+  return isOpen ? <Modal>{children}</Modal> : null;
 }
 
 Overlay.propTypes = {
   children: PropTypes.any,
   close: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
+  isOpen: PropTypes.bool.isRequired,
 };
