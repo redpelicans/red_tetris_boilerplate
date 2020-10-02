3271c075d4bd2be432cdf91d9e47142fbeb3406b
Jeremie <jeremiegthr@gmail.com>
2020-10-02 08:54:41 +0200

    test edge case on randomPick

--- test/client/helpers/randomPick.test.js
+++ test/client/helpers/randomPick.test.js
@@ -1,8 +1,15 @@
 import { randomPick } from "client/helpers/common";
 
-test("randomPick() should be an item from the list", () => {
+test("randomPick() with filled list should be an item from the list", () => {
   const list = ["Sherlock", "Watson", "Moriarty", "Molly", "Mary"];
   const randomValue = randomPick(list);
 
   expect(randomValue).toBeInArray(list);
+});
+
+test("randomPick() with empty list should be undefined", () => {
+  const list = [];
+  const randomValue = randomPick(list);
+
+  expect(randomValue).toBeUndefined();
 });
