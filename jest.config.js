69641ada138c4e2e622eb1a3edfbd2e48343c263
Jeremie <jeremiegthr@gmail.com>
2020-10-02 11:40:49 +0200

    set module dir to client directly

--- jest.config.js
+++ jest.config.js
@@ -71,7 +71,7 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: ["node_modules", "src"],
+  moduleDirectories: ["node_modules", "src/client"],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
