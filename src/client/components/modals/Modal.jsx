da48578bc89684d877c5b2c2120121159c4bd83a
Jeremie <jeremiegthr@gmail.com>
2020-10-02 12:11:36 +0200

    add rest prop to modal

--- src/client/components/modals/Modal.jsx
+++ src/client/components/modals/Modal.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import FlexBox from "components/flexbox/FlexBox";
 import PropTypes from "prop-types";
 
-export default function Modal({ children, className = "" }) {
+export default function Modal({ children, className = "", ...rest }) {
   return (
     <FlexBox className="modal fixed inset-0 items-center justify-center z-50">
       <div className="absolute inset-0 bg-black opacity-25" />
-      <div className={"relative " + className}>{children}</div>
+      <div className={"relative " + className} {...rest}>
+        {children}
+      </div>
     </FlexBox>
   );
 }
