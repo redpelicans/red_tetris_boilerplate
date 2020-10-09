761253ff52a2261425e22df4d75d5e2e829fa9ad
Jeremie <jeremiegthr@gmail.com>
2020-10-09 20:11:22 +0200

    add test

---
+++ test/client/hooks/tetris-game/useGravity.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { renderHook } from "@testing-library/react-hooks";
+import useGravity from "hooks/tetris-game/useGravity";
+import { GameContextProvider } from "store/layers/game";
+import { INTERVAL_MS } from "constants/tetris";
+
+describe("useAudio", () => {
+  const useContextStub = jest.spyOn(React, "useContext");
+  const useMemoStub = jest.spyOn(React, "useMemo");
+
+  test("should compute the gravity time interval", () => {
+    const wrapper = ({ children }) => (
+      <GameContextProvider>{children}</GameContextProvider>
+    );
+    const { result } = renderHook(() => useGravity(), { wrapper });
+
+    expect(useContextStub).toHaveBeenCalled();
+    expect(useMemoStub).toHaveBeenCalled();
+    expect(result.current).toEqual(INTERVAL_MS);
+  });
+});
