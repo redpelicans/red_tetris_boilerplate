156d92ba9d432b8b5896e106746cccddc2a33c95
Jeremie <jeremiegthr@gmail.com>
2020-10-22 13:14:25 +0200

    update reducer/action in consequence

--- src/client/actions/game.js
+++ src/client/actions/game.js
@@ -1,33 +1,7 @@
-export const FETCH_PIECES = "FETCH_PIECES";
-export const PULL_CURRENT_PIECE_FROM_NEXT_PIECES =
-  "PULL_CURRENT_PIECE_FROM_NEXT_PIECES";
-export const UPDATE_GRID = "UPDATE_GRID";
-export const UPDATE_CURRENT_PIECE = "UPDATE_CURRENT_PIECE";
 export const SET_PLAYER_IS_ALIVE = "SET_PLAYER_IS_ALIVE";
 export const SET_SCORE = "SET_SCORE";
 export const INCREASE_SPEED_RATE = "INCREASE_SPEED_RATE";
 export const INCREASE_ROWS_REMOVED = "INCREASE_ROWS_REMOVED";
-
-export const pushNewPiece = (newPieces) => ({
-  type: FETCH_PIECES,
-  newPieces,
-});
-
-let autoIncrementId = 0;
-export const pullCurrentPiece = () => ({
-  type: PULL_CURRENT_PIECE_FROM_NEXT_PIECES,
-  id: autoIncrementId++,
-});
-
-export const updateGrid = (newGrid) => ({
-  type: UPDATE_GRID,
-  newGrid,
-});
-
-export const updateCurrentPiece = (newPiece) => ({
-  type: UPDATE_CURRENT_PIECE,
-  newPiece,
-});
 
 export const setPlayerIsAlive = (alive) => ({
   type: SET_PLAYER_IS_ALIVE,
