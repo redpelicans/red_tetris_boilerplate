c9e5c773439dcacd48073fa26deb578ffb7b43c0
Jeremie <jeremiegthr@gmail.com>
2020-11-07 22:06:55 +0100

    remove hover effect and make all the lobby div clickable

--- src/client/pages/lobbies/LobbyItem.jsx
+++ src/client/pages/lobbies/LobbyItem.jsx
@@ -3,12 +3,10 @@ import FlexBox from "components/flexbox/FlexBox";
 import { LOBBY } from "../../../config/actions/lobby";
 import { socket } from "store/middleware/sockets";
 
-export default function LobbyItem({ lobby, state, dispatch }) {
-  const [hover, setHover] = React.useState(false);
-
+export default function LobbyItem({ lobby, state }) {
   const subscribeLobby = (lobbyId) => {
     socket.emit(LOBBY.SUBSCRIBE, {
-      lobbyId: lobbyId,
+      lobbyId,
       playerId: state.player.id,
     });
   };
@@ -16,34 +14,27 @@ export default function LobbyItem({ lobby, state, dispatch }) {
   const isFull = lobby.players.length >= lobby.maxPlayer;
 
   return (
-    <>
-      <FlexBox
-        direction="row"
-        className="lobby-item"
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-        <FlexBox direction="col" className="pl-8">
-          <span className="text-base font-semibold">{lobby.name}</span>
-          <span className="text-xs">{lobby.owner.name}</span>
-        </FlexBox>
-
-        {hover ? (
-          <button onClick={() => subscribeLobby(lobby.id)}>Join lobby</button>
-        ) : (
-          <FlexBox>
-            {lobby.isPlaying && <span className="mr-6">PLAYING</span>}
-            <span className="mr-2">
-              {lobby.players.length}/{lobby.maxPlayer}
-            </span>
-            {isFull || lobby.isPlaying ? (
-              <div className="status-dot bg-red-500" />
-            ) : (
-              <div className="status-dot bg-green-500" />
-            )}
-          </FlexBox>
-        )}
+    <FlexBox
+      direction="row"
+      className="lobby-item cursor-pointer"
+      onClick={() => subscribeLobby(lobby.id)}
+    >
+      <FlexBox direction="col" className="pl-8">
+        <span className="text-base font-semibold">{lobby.name}</span>
+        <span className="text-xs">{lobby.owner.name}</span>
       </FlexBox>
-    </>
+
+      <FlexBox>
+        {lobby.isPlaying && <span className="mr-6">PLAYING</span>}
+        <span className="mr-2">
+          {lobby.players.length}/{lobby.maxPlayer}
+        </span>
+        <div
+          className={`status-dot ${
+            isFull || lobby.isPlaying ? "bg-red-500" : "bg-green-500"
+          }`}
+        />
+      </FlexBox>
+    </FlexBox>
   );
 }
