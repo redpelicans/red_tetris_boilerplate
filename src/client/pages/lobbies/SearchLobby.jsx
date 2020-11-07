2144e446b0c2c29c48fa02f0f1e40f0a7b9caa0e
Jeremie <jeremiegthr@gmail.com>
2020-11-07 23:01:03 +0100

    add lobbies filtering logic

--- src/client/pages/lobbies/SearchLobby.jsx
+++ src/client/pages/lobbies/SearchLobby.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import FlexBox from "components/flexbox/FlexBox";
 
-export default function SearchLobby() {
+export default function SearchLobby({ searchedValue, setSearchedValue }) {
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setSearchedValue(newValue);
+  };
+
   return (
     <FlexBox
       direction="row"
       className="rounded-lg shadow-lg p-3 border border-grey-200"
     >
-      <input placeholder="Search a lobby..." className="pl-8 w-full h-auto" />
+      <input
+        placeholder="Search a lobby..."
+        className="pl-8 w-full h-auto"
+        value={searchedValue}
+        onChange={handleChange}
+      />
     </FlexBox>
   );
 }
