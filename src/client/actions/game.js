8c970c59bad58ede9fb7762b63e58068af1e3b07
Jeremie <jeremiegthr@gmail.com>
2020-11-11 11:55:25 +0100

    remove owner action

--- src/client/actions/game.js
+++ src/client/actions/game.js
@@ -1,7 +1,4 @@
 export const SET_PLAYER_IS_ALIVE = "SET_PLAYER_IS_ALIVE";
-export const SET_OWNER_SCORE = "SET_OWNER_SCORE";
-export const INCREASE_SPEED_RATE = "INCREASE_SPEED_RATE";
-export const INCREASE_ROWS_REMOVED = "INCREASE_ROWS_REMOVED";
 export const SET_GAME = "SET_GAME";
 export const SET_SCORE = "SET_SCORE";
 export const SET_BOARD = "SET_BOARD";
