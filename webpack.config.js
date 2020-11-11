6702dd8bf915db4c30c7dd9569e63bd2d64103ef
nicolasvienot <nvienot@student.42.fr>
2020-11-11 04:58:19 +0100

    configures file-loader in webpack in order tu use require on img

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,14 @@ module.exports = () => {
         {
           test: /\.(png|svg|jpe?g|gif)$/,
           include: path.resolve(CLIENT_DIR, "assets/img"),
-          use: require.resolve("file-loader"),
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                esModule: false,
+              },
+            },
+          ],
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
